69be216214bd05458e2444a575a8de6bd05ba65a
Andrej A Antonov <polymorphm@gmail.com>
2012-12-27 09:02:11 +0400

    printing 'user error: ...' (cherry picked from commit 6fd36169090b3a96f59a1e860476976d4ce803d1)

--- lib_verif_scheduler_2012_12_25/main_cli.py
+++ lib_verif_scheduler_2012_12_25/main_cli.py
@@ -17,65 +17,70 @@
 
 assert str is not bytes
 
+import sys
 import argparse
-from . import date_parser, excl_file
+from . import date_parser
+from . import excl_file
 from . import verif_scheduler
 
 class UserError(Exception):
     pass
 
 def main():
-    parser = argparse.ArgumentParser(
-            description='utility for scheduling device verification process')
-    parser.add_argument('--begin-date', metavar='BEGIN-DATE',
-            help='date of period begin')
-    parser.add_argument('--end-date', metavar='END-DATE',
-            help='date of period end (not including end day)')
-    parser.add_argument('--verif-count', metavar='VERIF-COUNT', type=int,
-            help='count of device verifications')
-    parser.add_argument('--week-days', metavar='WEEK-DAYS-LIST',
-            help='list of week days. example: "0,2,4" means is "mo, we, fr"')
-    parser.add_argument('--excl', metavar='EXCLUSION-FILE-PATH',
-            help='path to exclusion file')
-    parser.add_argument('--use-rus-fmt', action='store_true',
-            help='use russian regional format for result output file')
-    parser.add_argument('--out', metavar='OUT-FILE-PATH',
-            help='path to result output file')
-    
-    args = parser.parse_args()
-    
-    if args.begin_date is None:
-        raise UserError('args.begin_date is None')
-    if args.end_date is None:
-        raise UserError('args.end_date is None')
-    if args.verif_count is None or args.verif_count <= 0:
-        raise UserError('args.verif_count is None or args.verif_count <= 0')
-    if args.week_days is None:
-        raise UserError('args.week_days is None')
-    if args.out is None:
-        raise UserError('args.out is None')
-    
-    begin_date = date_parser.parse_date(args.begin_date)
-    end_date = date_parser.parse_date(args.end_date)
-    week_days = date_parser.parse_week_days(args.week_days)
-    if args.excl is not None:
-        excl_list = tuple(excl_file.load_excl_file(args.excl))
-    else:
-        excl_list = None
-    
-    sch_dates = verif_scheduler.verif_schedule(
-            verif_scheduler.get_dates(
-                    begin_date,
-                    end_date,
-                    week_days,
-                    excl_list=excl_list,
-                    ),
-            args.verif_count,
-            )
-    
-    with open(args.out, 'w', encoding='utf-8', newline='\n', errors='replace') as fd:
-        for sch_date in sch_dates:
-            if args.use_rus_fmt:
-                fd.write('{}\n'.format(sch_date.strftime('%d.%m.%Y')))
-                continue
-            fd.write('{}\n'.format(sch_date))
+    try:
+        parser = argparse.ArgumentParser(
+                description='utility for scheduling device verification process')
+        parser.add_argument('--begin-date', metavar='BEGIN-DATE',
+                help='date of period begin')
+        parser.add_argument('--end-date', metavar='END-DATE',
+                help='date of period end (not including end day)')
+        parser.add_argument('--verif-count', metavar='VERIF-COUNT', type=int,
+                help='count of device verifications')
+        parser.add_argument('--week-days', metavar='WEEK-DAYS-LIST',
+                help='list of week days. example: "0,2,4" means is "mo, we, fr"')
+        parser.add_argument('--excl', metavar='EXCLUSION-FILE-PATH',
+                help='path to exclusion file')
+        parser.add_argument('--use-rus-fmt', action='store_true',
+                help='use russian regional format for result output file')
+        parser.add_argument('--out', metavar='OUT-FILE-PATH',
+                help='path to result output file')
+        
+        args = parser.parse_args()
+        
+        if args.begin_date is None:
+            raise UserError('args.begin_date is None')
+        if args.end_date is None:
+            raise UserError('args.end_date is None')
+        if args.verif_count is None or args.verif_count <= 0:
+            raise UserError('args.verif_count is None or args.verif_count <= 0')
+        if args.week_days is None:
+            raise UserError('args.week_days is None')
+        if args.out is None:
+            raise UserError('args.out is None')
+        
+        begin_date = date_parser.parse_date(args.begin_date)
+        end_date = date_parser.parse_date(args.end_date)
+        week_days = date_parser.parse_week_days(args.week_days)
+        if args.excl is not None:
+            excl_list = tuple(excl_file.load_excl_file(args.excl))
+        else:
+            excl_list = None
+        
+        sch_dates = verif_scheduler.verif_schedule(
+                verif_scheduler.get_dates(
+                        begin_date,
+                        end_date,
+                        week_days,
+                        excl_list=excl_list,
+                        ),
+                args.verif_count,
+                )
+        
+        with open(args.out, 'w', encoding='utf-8', newline='\n', errors='replace') as fd:
+            for sch_date in sch_dates:
+                if args.use_rus_fmt:
+                    fd.write('{}\n'.format(sch_date.strftime('%d.%m.%Y')))
+                    continue
+                fd.write('{}\n'.format(sch_date))
+    except UserError as e:
+        print('user error: {}'.format(e), file=sys.stderr)
